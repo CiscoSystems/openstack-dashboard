ef26d8d0c65520d665921ff7d24b2ec213bf4e6a
Mark Gius <launchpad@markgius.com>
2011-06-08 15:54:55 -0700

    More tests implemented

--- django-openstack/src/django_openstack/nova/tests/manager_tests.py
+++ django-openstack/src/django_openstack/nova/tests/manager_tests.py
@@ -12,6 +12,8 @@ from nova_adminclient import client as nova_client
 
 
 TEST_IMAGE_ID = 1
+TEST_INSTANCE_ID = 1
+TEST_INSTANCE_IDS = range(8)
 TEST_PROJECT_NAME = 'testProject'
 TEST_PROJECT_DESCRIPTION = 'testDescription'
 TEST_PROJECT_MANAGER_ID = 100
@@ -35,16 +37,15 @@ class ProjectManagerTests(test.TestCase):
         self.mox.UnsetStubs()
 
     def stub_conn_mock(self, count=1):
-        ''' 
+        '''
         Stubs get_openstack_connection as an EC2Connection and returns the
-        EC2Connection mock 
+        EC2Connection mock
         '''
         self.mox.StubOutWithMock(self.manager, 'get_openstack_connection')
         conn_mock = self.mox.CreateMock(EC2Connection)
         for i in range(count):
             self.manager.get_openstack_connection().AndReturn(conn_mock)
         return conn_mock
-
 
     def test_get_openstack_connection(self):
         self.mox.StubOutWithMock(connection, 'get_nova_admin_connection')
@@ -75,23 +76,23 @@ class ProjectManagerTests(test.TestCase):
     def test_get_images(self):
         ''' TODO: Need to figure out what I'm doing here...'''
         self.assertTrue(False)
-        TEST_IMAGE_IDS = [TEST_IMAGE_ID,TEST_IMAGE_ID + 1]
+        TEST_IMAGE_IDS = [TEST_IMAGE_ID, TEST_IMAGE_ID + 1]
         self.mox.StubOutwithMock(self.manager, 'get_openstack_connection')
         conn_mock = self.mox.CreateMock(EC2Connection)
         images_mock = self.mox.CreateMockAnything()
-        
 
     def test_get_image(self):
         TEST_IMAGE_BAD_ID = TEST_IMAGE_ID + 1
         self.mox.StubOutWithMock(self.manager, 'get_images')
-        self.manager.get_images(image_ids=[TEST_IMAGE_ID]).AndReturn([TEST_IMAGE_ID])
+        self.manager.get_images(image_ids=[TEST_IMAGE_ID]).AndReturn(
+                [TEST_IMAGE_ID])
         self.manager.get_images(image_ids=[TEST_IMAGE_BAD_ID]).AndReturn([])
 
         self.mox.ReplayAll()
 
         image_result = self.manager.get_image(TEST_IMAGE_ID)
         self.assertEqual(TEST_IMAGE_ID, image_result)
-        
+
         image_result = self.manager.get_image(TEST_IMAGE_BAD_ID)
         self.assertTrue(image_result is None)
 
@@ -109,7 +110,6 @@ class ProjectManagerTests(test.TestCase):
 
         self.mox.VerifyAll()
 
-
     def test_update_image(self):
         TEST_DISPLAY_NAME = 'testDisplayName'
         TEST_DESCRIPTION = 'testDescription'
@@ -117,8 +117,8 @@ class ProjectManagerTests(test.TestCase):
 
         conn_mock = self.stub_conn_mock(count=2)
 
-        # TODO: Figure out why the arg parsing isn't working... 
-        conn_mock.get_object('UpdateImage', 
+        # TODO: Figure out why the arg parsing isn't working...
+        conn_mock.get_object('UpdateImage',
                              #And(In(TEST_IMAGE_ID), In(TEST_DISPLAY_NAME),
                              #    In(TEST_DESCRIPTION)),
                              IgnoreArg(),
@@ -130,8 +130,8 @@ class ProjectManagerTests(test.TestCase):
 
         self.mox.ReplayAll()
 
-        update_result = self.manager.update_image(TEST_IMAGE_ID, 
-                                  display_name=TEST_DISPLAY_NAME, 
+        update_result = self.manager.update_image(TEST_IMAGE_ID,
+                                  display_name=TEST_DISPLAY_NAME,
                                   description=TEST_DESCRIPTION)
 
         self.assertEqual(update_result, TEST_RETURN)
@@ -151,25 +151,27 @@ class ProjectManagerTests(test.TestCase):
         conn_mock = self.stub_conn_mock(count=2)
 
         # Test: default attributes passed
-        conn_mock.modify_image_attribute(TEST_IMAGE_ID, 
-                                         attribute=None, 
-                                         operation=None, 
+        conn_mock.modify_image_attribute(TEST_IMAGE_ID,
+                                         attribute=None,
+                                         operation=None,
                                          groups='all').AndReturn(TEST_RETURN)
         # Test: custom attributes passed
-        conn_mock.modify_image_attribute(TEST_IMAGE_ID, 
-                                         attribute=TEST_ATTRIBUTE, 
-                                         operation=TEST_OPERATION, 
-                                         groups=TEST_GROUPS).AndReturn(TEST_RETURN)
+        conn_mock.modify_image_attribute(TEST_IMAGE_ID,
+                                         attribute=TEST_ATTRIBUTE,
+                                         operation=TEST_OPERATION,
+                                         groups=TEST_GROUPS
+                                        ).AndReturn(TEST_RETURN)
 
         self.mox.ReplayAll()
 
         modify_return = self.manager.modify_image_attribute(TEST_IMAGE_ID)
         self.assertEqual(modify_return, TEST_RETURN)
 
-        modify_return = self.manager.modify_image_attribute(TEST_IMAGE_ID,
-                                                            attribute=TEST_ATTRIBUTE,
-                                                            operation=TEST_OPERATION,
-                                                            groups=TEST_GROUPS)
+        modify_return = \
+                self.manager.modify_image_attribute(TEST_IMAGE_ID,
+                                                    attribute=TEST_ATTRIBUTE,
+                                                    operation=TEST_OPERATION,
+                                                    groups=TEST_GROUPS)
         self.assertEqual(modify_return, TEST_RETURN)
 
         self.mox.VerifyAll()
@@ -184,8 +186,8 @@ class ProjectManagerTests(test.TestCase):
                                 user_data='userData').AndReturn(TEST_RETURN)
 
         self.mox.ReplayAll()
-        
-        run_return = self.manager.run_instances(TEST_IMAGE_ID, 
+
+        run_return = self.manager.run_instances(TEST_IMAGE_ID,
                                                 key_name='testKey',
                                                 user_data='userData')
         self.assertEqual(run_return, TEST_RETURN)
@@ -193,13 +195,13 @@ class ProjectManagerTests(test.TestCase):
         self.mox.VerifyAll()
 
     def test_get_instance_count(self):
-        TEST_LEN = 5
         self.mox.StubOutWithMock(self.manager, 'get_instances')
 
         def valid_instance_list():
-            self.manager.get_instances().AndReturn([i for i in range(TEST_LEN)])
+            self.manager.get_instances().AndReturn(TEST_INSTANCE_IDS)
             self.mox.ReplayAll()
-            self.assertEqual(TEST_LEN, self.manager.get_instance_count())
+            self.assertEqual(len(TEST_INSTANCE_IDS),
+                             self.manager.get_instance_count())
             self.mox.VerifyAll()
             self.mox.ResetAll()
 
@@ -213,13 +215,115 @@ class ProjectManagerTests(test.TestCase):
         valid_instance_list()
         invalid_instance_list()
 
+    def get_reservation_mock(self, instance_ids):
+        rMock = self.mox.CreateMock(boto.ec2.instance.Reservation)
+        instances = []
+        for instance_id in instance_ids:
+            instance = boto.ec2.instance.Instance()
+            instance.id = instance_id
+            instances.append(instance)
+        rMock.instances = instances
+
+        return rMock
+
+    def get_single_instance_single_reservation(self, instance_id):
+        return self.get_reservation_mock([instance_id])
+
+    def get_multiple_instances_multiple_reservations(self, instance_ids):
+        rMock1 = \
+            self.get_reservation_mock(instance_ids[:len(instance_ids) // 2])
+        rMock2 = \
+            self.get_reservation_mock(instance_ids[len(instance_ids) // 2:])
+        return [rMock1, rMock2]
+
     def test_get_instances(self):
-        """docstring for get_instances"""
-        pass
+
+        def single_instance_single_reservation():
+            conn_mock = self.stub_conn_mock()
+            rMock = \
+                self.get_single_instance_single_reservation(TEST_INSTANCE_ID)
+            conn_mock.get_all_instances().AndReturn([rMock])
+
+            self.mox.ReplayAll()
+
+            instances = self.manager.get_instances()
+            self.assertEqual(1, len(instances))
+            self.assertEqual(TEST_INSTANCE_ID, instances[0].id)
+
+            self.mox.VerifyAll()
+            self.mox.ResetAll()
+
+        def multiple_instances_multiple_reservations():
+            conn_mock = self.stub_conn_mock()
+            rMocks = self.get_multiple_instances_multiple_reservations(
+                        TEST_INSTANCE_IDS)
+
+            conn_mock.get_all_instances().AndReturn(rMocks)
+
+            self.mox.ReplayAll()
+
+            instances = self.manager.get_instances()
+            self.assertEqual(len(TEST_INSTANCE_IDS), len(instances))
+            instance_ids = [instance.id for instance in instances]
+            # Upgrade to 2.7 to make this work
+            # self.assertItemsEqual(instance_ids, TEST_INSTANCE_IDS)
+            for instance_id in instance_ids:
+                self.assertTrue(instance_id in TEST_INSTANCE_IDS)
+
+            self.mox.VerifyAll()
+            self.mox.ResetAll()
+
+        single_instance_single_reservation()
+        multiple_instances_multiple_reservations()
 
     def test_get_instance(self):
-        """docstring for get_instance"""
-        pass
+
+        def single_instance_single_reservation():
+            conn_mock = self.stub_conn_mock()
+            rMock = \
+                self.get_single_instance_single_reservation(TEST_INSTANCE_ID)
+            conn_mock.get_all_instances().AndReturn([rMock])
+
+            self.mox.ReplayAll()
+
+            instance = self.manager.get_instance(TEST_INSTANCE_ID)
+            self.assertEqual(instance.id, TEST_INSTANCE_ID)
+
+            self.mox.VerifyAll()
+            self.mox.UnsetStubs()
+
+        def multiple_instances_multiple_reservations():
+            INDEX = 4
+            conn_mock = self.stub_conn_mock()
+            rMocks = self.get_multiple_instances_multiple_reservations(
+                        TEST_INSTANCE_IDS)
+            conn_mock.get_all_instances().AndReturn(rMocks)
+
+            self.mox.ReplayAll()
+
+            instance = self.manager.get_instance(TEST_INSTANCE_IDS[INDEX])
+            self.assertEqual(instance.id, TEST_INSTANCE_IDS[INDEX])
+
+            self.mox.VerifyAll()
+            self.mox.UnsetStubs()
+
+        def instance_missing():
+            conn_mock = self.stub_conn_mock()
+            rMock = \
+                self.get_single_instance_single_reservation(TEST_INSTANCE_ID)
+            conn_mock.get_all_instances().AndReturn([rMock])
+
+            self.mox.ReplayAll()
+
+            instance = self.manager.get_instance(TEST_INSTANCE_ID + 1)
+            self.assertTrue(instance is None)
+
+            self.mox.VerifyAll()
+            self.mox.UnsetStubs()
+
+        single_instance_single_reservation()
+        multiple_instances_multiple_reservations()
+        instance_missing()
 
     def test_update_instance(self):
         pass
